7c91720ef600c6067a5d0cef593bff4a2991bf09
Ollie <olliejday@gmail.com>
2021-02-02 15:11:43 +0000

    merged with tim

--- python/onitama/tests/test_env.py
+++ python/onitama/tests/test_env.py
@@ -4,22 +4,22 @@ from onitama.game import get_move
 import numpy as np
 import unittest
 
+
 class RewardTests(unittest.TestCase):
+    def testMoveForwardsPlayer2(self):
+        # Test move_forwards
+        ##Move player 2 pawn (id=0) forward 2 squares
+        ##Reward should = 0.2 (2 moves * 0.1)
+        env = OnitamaEnv(player=2)
 
-	def testMoveForwardsPlayer2(self):
-		#Test move_forwards
-    	##Move player 2 pawn (id=0) forward 2 squares
-    	##Reward should = 0.2 (2 moves * 0.1)
-    	env = OnitamaEnv(player=2)
+        moveJson = {"pos": [2, 0],
+                    "name": "pawn",
+                    "i": 0,
+                    "id": 0}
 
-    	moveJson = {"pos":[2,0],
-    			"name":"pawn",
-    			"i":0,
-    			"id":0}
-
-    	move = Move(moveJson)
-    	env.game.player2.step(move,None)
-   		self.assertEqual(env.get_reward,0.2)
+        move = Move(moveJson)
+        env.game.player2.step(move, None)
+        self.assertEqual(env.get_reward, 0.2)
 
 
 class EnvTest(unittest.TestCase):
