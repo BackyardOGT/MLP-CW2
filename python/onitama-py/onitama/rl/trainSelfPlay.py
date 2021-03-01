d2f1d02601e7780c33eaf132d1f0fa408618dbb2
Ollie <olliejday@gmail.com>
2021-03-01 18:35:06 +0000

    changed ppo logging

--- python/onitama-py/onitama/rl/trainSelfPlay.py
+++ python/onitama-py/onitama/rl/trainSelfPlay.py
@@ -6,29 +6,15 @@ from stable_baselines.common.callbacks import CheckpointCallback, EvalCallback, 
 import argparse
 
 
-def train_rl(seed, policy):
-    eval_env = OnitamaEnv(seed, SimpleAgent, verbose=False)
-
-    checkpoint_callback = CheckpointCallback(save_freq=1e4, save_path='./logs/',
-                                             name_prefix='rl_model', verbose=2)
-    eval_policy_cb = EvalCB()
-    eval_callback = EvalCallback(eval_env, best_model_save_path='./logs/',
-                             log_path='./logs/', eval_freq=int(1e3),
-                             deterministic=True, render=False,
-                                 evaluate_policy_callback=eval_policy_cb)
-    callback = CallbackList([checkpoint_callback, eval_callback])
-    policy.learn(int(1e6), callback=callback)
-
-
 def getPolicy(algorithm, seed):
     env = OnitamaSelfPlayEnv(seed, verbose=False)
 
     if algorithm == "PPO":
         policy = PPO2(ACMaskedCNNPolicy,
-                     env,
-                     seed=seed,
-                     verbose=1,
-                     )
+                      env,
+                      seed=seed,
+                      verbose=1,
+                      )
 
     else:
         policy = DQN(DQNMaskedCNNPolicy,
@@ -42,12 +28,25 @@ def getPolicy(algorithm, seed):
     return policy
 
 
+def train_rl(algorithm, seed):
+    policy = getPolicy(algorithm, seed)
+    eval_env = OnitamaEnv(seed, SimpleAgent, verbose=False)
+
+    checkpoint_callback = CheckpointCallback(save_freq=1e4, save_path='./logs/',
+                                             name_prefix='rl_model', verbose=2)
+    eval_policy_cb = EvalCB()
+    eval_callback = EvalCallback(eval_env, best_model_save_path='./logs/',
+                                 log_path='./logs/', eval_freq=int(1e3),
+                                 deterministic=True, render=False,
+                                 evaluate_policy_callback=eval_policy_cb)
+    callback = CallbackList([checkpoint_callback, eval_callback])
+    policy.learn(int(1e6), callback=callback, log_interval=10 if algorithm == "PPO" else 500)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--seed', default=12314, type=int)
     parser.add_argument('--algorithm', default="PPO", type=str)
     args = parser.parse_args()
 
-    policy = getPolicy(args.algorithm, args.seed)
-
-    train_rl(args.seed, policy)
+    train_rl(args.algorithm, args.seed)
