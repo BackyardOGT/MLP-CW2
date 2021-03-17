8e6f2788d14846c624d77e4b0a4c7d913534d47b
Ollie <olliejday@gmail.com>
2021-03-17 15:28:46 +0000

    improved logging

--- python/onitama-py/onitama/rl/train.py
+++ python/onitama-py/onitama/rl/train.py
@@ -1,8 +1,11 @@
+import os
+from datetime import datetime
 from onitama.rl import DQNMaskedCNNPolicy, ACMaskedCNNPolicy, SimpleAgent, RandomAgent
 from onitama.rl.callbacks import EvalCB
 from stable_baselines import DQN, PPO2
 from stable_baselines.common.evaluation import evaluate_policy
 from stable_baselines.common.callbacks import CheckpointCallback, EvalCallback, CallbackList
+from stable_baselines.bench.monitor import Monitor
 import numpy as np
 import argparse
 import onitama
@@ -13,7 +16,8 @@ def train_rl(seed, algorithm):
     env = gym.make("Onitama-v0", seed=seed, agent_type=agent_type, verbose=False)
     eval_env = gym.make("Onitama-v0", seed=seed, agent_type=agent_type, verbose=False)
     if algorithm == "PPO":
-        logdir = "./logs/ppo-tb/"
+        basedir = "./logs/ppo-tb/"
+        env, logdir = setup_monitor(basedir, env)
         policy = PPO2(ACMaskedCNNPolicy,
                       env,
                       seed=seed,
@@ -22,7 +26,8 @@ def train_rl(seed, algorithm):
                       )
 
     else:
-        logdir = "./logs/dqn-tb/"
+        basedir = "./logs/dqn-tb/"
+        env, logdir = setup_monitor(basedir, env)
         policy = DQN(DQNMaskedCNNPolicy,
                      env,
                      seed=seed,
@@ -31,15 +36,23 @@ def train_rl(seed, algorithm):
                      tensorboard_log=logdir
                      )
 
-    checkpoint_callback = CheckpointCallback(save_freq=5e3, save_path='./logs/',
+    checkpoint_callback = CheckpointCallback(save_freq=5e3, save_path=logdir,
                                              name_prefix='rl_model', verbose=2)
     eval_policy_cb = EvalCB(logdir)
-    eval_callback = EvalCallback(eval_env, best_model_save_path='./logs/',
+    eval_callback = EvalCallback(eval_env, best_model_save_path=logdir,
                              log_path='./logs/', eval_freq=500, n_eval_episodes=20,
                              deterministic=True, render=False,
                                  evaluate_policy_callback=eval_policy_cb)
     callback = CallbackList([checkpoint_callback, eval_callback])
     policy.learn(int(1e6), callback=callback)
+
+
+def setup_monitor(basedir, env):
+    logdir = basedir + datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p")
+    if not os.path.exists(basedir): os.mkdir(basedir)
+    if not os.path.exists(logdir): os.mkdir(logdir)
+    env = Monitor(env, logdir + "/logs")
+    return env, logdir
 
 
 if __name__ == "__main__":
